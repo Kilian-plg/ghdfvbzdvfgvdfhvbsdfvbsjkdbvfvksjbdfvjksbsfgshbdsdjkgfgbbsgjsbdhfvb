a43754643c2167d11f932ff1dffe708695b1f963
Kilian Pligersdorffer <kilian.pligersdorffer@epita.fr>
2025-01-16 23:32:48 +0100

    enfin

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public NavMeshAgent agent2; 
+    public float stopDistance = 2.5F; // Distance minimale avant d'arrêter la poursuite
+
+    private Vector3 lastTargetPosition;
+
+    void Update()
+    {
+        if (PlayerController.instance != null)
+        {
+            // Récupérer la position actuelle du joueur
+            Vector3 targetPos = PlayerController.instance.GetPlayerPosition();
+
+            // Vérifier la distance entre l'agent et le joueur
+            float distanceToPlayer = Vector3.Distance(agent2.transform.position, targetPos);
+
+            // Si la distance est supérieure à la distance minimale ou que la cible a changé, recalculer le chemin
+            if (distanceToPlayer > stopDistance || targetPos != lastTargetPosition)
+            {
+                agent2.SetDestination(targetPos);
+                lastTargetPosition = targetPos; 
+            }
+            else
+            {
+                // Arrêter le mouvement si l'agent est dans la portée minimale
+                agent2.ResetPath(); 
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+	public static PlayerController instance;
+
+  	private void Awake()
+    {
+        instance = this;
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        return agent != null ? agent.transform.position : transform.position; // Retourne la position actuelle du joueur
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+			Inventory.instance.addRune(1);
+			Inventory.instance.addPotion(1);
+            HealthBar.instance.SetActualHealth(20);
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Inventory.instance.addPotion(-1);
+            HealthBar.instance.SetActualHealth(-20);
+        }
+    }
+}
